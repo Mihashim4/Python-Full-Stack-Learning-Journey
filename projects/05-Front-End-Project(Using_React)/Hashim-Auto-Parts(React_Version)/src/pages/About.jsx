1424d18abb595f6e9a827a473a67e77533fe2a5b
Mihashim4 <mihashim4@gmail.com>
2026-08-13 23:43:23 +0530

    Added Hashim Auto Parts React project

---
+++ projects/05-Front-End-Project(Using_React)/Hashim-Auto-Parts(React_Version)/src/pages/About.jsx
@@ -0,0 +1,42 @@
+function About() {
+  return (
+    <div className="fade-in">
+      <section className="about-hero">
+        <h1>Our Story</h1>
+      </section>
+
+      <section className="story py-5">
+        <div className="container">
+        <div className="story-content">
+          <p>
+            This website is a tribute to my grandfather's old shop —{" "}
+            <strong>HASHIM Auto Parts & Old Motor Spare Parts</strong>.
+          </p>
+
+          <p>
+            For many years, the shop was a trusted place in Marthandam where
+            people could find new and old spare parts for cars, lorries, autos,
+            and more. From engine parts to bearings, tires, and electrical
+            components — my grandfather helped many customers keep their
+            vehicles running.
+          </p>
+
+          <p>
+            Though the physical shop is no longer there, the hard work, honesty,
+            and service it provided remain in our memories.
+          </p>
+
+          <p>
+            This website is created in loving memory of that small but important
+            business that served the community for a long time.
+          </p>
+        </div>
+        </div>
+      </section>
+       
+       
+    </div>
+  );
+}
+
+export default About;
