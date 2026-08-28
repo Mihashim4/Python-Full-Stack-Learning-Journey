98a3609556d827d949f0598e2650d82836ef605e
Mihashim4 <mihashim4@gmail.com>
2026-08-28 17:12:46 +0530

    added Python Roadmap

---
+++ projects/06-Python-Works-Roadmap/01-Grade-Calculator/Grade_calculator.py
@@ -0,0 +1,13 @@
+mark = int(input("Enter your mark:    "))
+if mark>=95<100:
+    print("You got an Excellent Mark of grade A+")
+elif mark>=80<95:
+    print("You got grade A")
+elif mark>=65<80:
+    print("You got grade B")
+elif mark>=45<65:
+    print("You got grade C")
+elif mark>=35<45:
+    print("You got grade D")
+else:
+    print("You have been failed")
---
+++ projects/06-Python-Works-Roadmap/02-Database-Username-Password-Identifiying-And-Continue-Login/Username_password.py
@@ -0,0 +1,14 @@
+Username = input("Enter your username :    ")
+Password = input("Enter your password :    ")
+if Username=="Mihashim4":
+    if Password=="1234567":
+        print("You can Login")
+    else:
+        print("You can't login")
+elif Username=="Hashim":
+    if Password=="12121212":
+        print("You can login")
+    else:
+        print("You can't login")
+else:
+    print("No Username Found")
